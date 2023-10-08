7e24a815203ae2b64914b96ccb73e29b0210ed27
victorseara <victorseara.me@gmail.com>
2023-10-08 13:18:01 -0300

    refactor(use-isomorphic-layout-effect): return a const instead of a function

--- packages/use-isomorphic-layout-effect/hook/use-isomorphic-layout-effect.ts
+++ packages/use-isomorphic-layout-effect/hook/use-isomorphic-layout-effect.ts
@@ -1,7 +1,6 @@
 import React from "react";
 
-export default function useIsomorphicLayoutEffect() {
-  return typeof window !== "undefined"
-    ? React.useLayoutEffect
-    : React.useEffect;
-}
+const useIsomorphicLayoutEffect =
+  typeof window !== "undefined" ? React.useLayoutEffect : React.useEffect;
+
+export default useIsomorphicLayoutEffect;
